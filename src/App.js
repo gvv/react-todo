d059e48f6a4a8c27ca648e92c36314ae0babacf3
gvv <memovargasv@gmail.com>
2019-08-31 12:15:11 -0500

    ToDo App commit 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,91 @@
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import Checkbox from '@material-ui/core/Checkbox';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+      todos: []
+    }
+  }
+
+updateValue = e => {
+  console.log(e.target.value);
+  this.setState({
+    value: e.target.value
+  })
+};
+
+/*
+saveTodo = () => {
+  this.setState({
+    value: '',
+    todos: [...this.state.todos, this.state.value]
+  });
+};
+*/
+
+saveTodo = () => {
+  if (this.state.value !== '') {
+  this.setState({
+    value: '',
+    todos: [...this.state.todos, this.state.value]
+  });
+}
+};
+
+render () {
+  console.log(this.state.todos);
   return (
-    <h1>Hola Mundo!</h1>
+    <React.Fragment>   {/* en lugar de un div vacio*/}
+    <Typography variant='h2' align='center' gutterBottom>
+      Lista To-Do
+    </Typography>
+    <Grid container justify='center'>
+      <Grid item>
+        <form onSubmit = { e => {
+          e.preventDefault();
+          this.saveTodo();
+        }}>
+        <TextField type='text' placeholder='New to do ...' value={this.state.value} onChange={this.updateValue}/>
+        </form>
+      </Grid>
+    </Grid>
+    <Grid container justify='center'>
+      <Grid item md={8}>
+        <List>
+          {
+            this.state.todos.map((item, index) => {
+              return(
+                <ListItem button key={index}> 
+                <Checkbox/>
+                <ListItemText primary={item}/>
+                <ListItemSecondaryAction>
+                  <IconButton>
+                    <DeleteIcon/>
+                  </IconButton>
+                </ListItemSecondaryAction>
+              </ListItem>
+              )
+            })
+          }
+          
+        </List>
+      </Grid>
+    </Grid>
+    </React.Fragment>
   );
+}
 }
 
 export default App;
